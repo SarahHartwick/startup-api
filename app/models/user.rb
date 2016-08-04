dcf8f534bb37ef77850f8bc667ba1e6612b17212
SarahHartwick <sarah@surfsetfitness.com>
2016-08-04 00:07:15 -0400

    Fix for User Startups table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 #
 class User < ActiveRecord::Base
   include Authentication
-  has_many :companies
   has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' },
   :default_url => "https://www.mautic.org/media/images/default_avatar.png", :storage => :s3, :bucket => "sarahhartwick"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
